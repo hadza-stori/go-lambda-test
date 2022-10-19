35f7b2716e0d5fa1371c80f494f33e9ce130ba8f
Isaac Osuna <eddieisaac@gmail.com>
2022-10-19 10:59:57 -0600

    SAM template changes to handle GIN events

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,2 @@
 sam build
-sam deploy \
-    --stack-name stori-go-hadza-test \
-    --region us-west-2 \
-    --capabilities CAPABILITY_IAM \
-    --s3-bucket dev-cf-code-deploy
+sam deploy --stack-name stori-go-hadza-test --region us-west-2 --capabilities CAPABILITY_IAM --s3-bucket dev-cf-code-deploy
